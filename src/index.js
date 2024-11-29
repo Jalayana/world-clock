6956f72399840c88952cf1355cc55297fe413f87
Jalayana <millerja14@outlook.com>
2024-11-29 17:53:03 +0800

    Added the functionality for the select

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 function updateTime() {
   //Beijing
   let beijingElement = document.querySelector("#beijing");
-  let beijingDateElement = beijingElement.querySelector(".date");
-  let beijingTimeDifferenceElement = document.querySelector(".time-difference");
-  let beijingTimeElement = beijingElement.querySelector(".time");
-  let beijingTime = moment().tz("Asia/Shanghai");
+  if (beijingElement) {
+    let beijingDateElement = beijingElement.querySelector(".date");
+    let beijingTimeDifferenceElement =
+      document.querySelector(".time-difference");
+    let beijingTimeElement = beijingElement.querySelector(".time");
+    let beijingTime = moment().tz("Asia/Shanghai");
 
-  beijingDateElement.innerHTML = beijingTime.format("MMMM Do, YYYY");
-  beijingTimeDifferenceElement.innerHTML = "3 hours ahead";
-  beijingTimeElement.innerHTML = beijingTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
+    beijingDateElement.innerHTML = beijingTime.format("MMMM Do, YYYY");
+    beijingTimeDifferenceElement.innerHTML = "3 hours ahead";
+    beijingTimeElement.innerHTML = beijingTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   //New York
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeDifferenceElement = document.querySelector(".time-difference");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeDifferenceElement =
+      document.querySelector(".time-difference");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
-  newYorkTimeDifferenceElement.innerHTML = "3 hours ahead";
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
+    newYorkTimeDifferenceElement.innerHTML = "3 hours ahead";
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
