842dc3ecaa82ba019c93aa805126a6850b722b6d
Jalayana <millerja14@outlook.com>
2024-12-01 21:12:34 +0800

    Added another element, cleaned up unused js code

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ function updateTime() {
   let beijingElement = document.querySelector("#beijing");
   if (beijingElement) {
     let beijingDateElement = beijingElement.querySelector(".date");
-    let beijingTimeDifferenceElement =
-      document.querySelector(".time-difference");
     let beijingTimeElement = beijingElement.querySelector(".time");
     let beijingTime = moment().tz("Asia/Shanghai");
 
     beijingDateElement.innerHTML = beijingTime.format("MMMM Do, YYYY");
-    beijingTimeDifferenceElement.innerHTML = "3 hours ahead";
     beijingTimeElement.innerHTML = beijingTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
@@ -18,14 +15,23 @@ function updateTime() {
   let newYorkElement = document.querySelector("#new-york");
   if (newYorkElement) {
     let newYorkDateElement = newYorkElement.querySelector(".date");
-    let newYorkTimeDifferenceElement =
-      document.querySelector(".time-difference");
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTime = moment().tz("America/New_York");
 
     newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
-    newYorkTimeDifferenceElement.innerHTML = "3 hours ahead";
     newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  //London
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
